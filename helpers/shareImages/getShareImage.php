24a42f9fba4888f29e89820ef7b3f5ea1fb939f4
Leo Mancini <mancini@leo.gd>
2020-04-19 13:19:45 -0700

    Updating root URL for share page

--- helpers/shareImages/getShareImage.php
+++ helpers/shareImages/getShareImage.php
@@ -2,10 +2,15 @@
     require('../functions.php');
     $config = loadConfig('../');
     
+    if ($_SERVER['HTTP_HOST'] === 'localhost' || $_SERVER['HTTP_HOST'] === 'leo.local') {
+        $rootURL = 'http://'.$_SERVER['HTTP_HOST'].'/nyc-neighborhood-quiz';
+    } else {
+        $rootURL = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https' : 'http').'://'.$_SERVER['HTTP_HOST'].'/';
+    }
+    
     $shareImageShortHashLookup = $_GET['shortHashLookup'];
 
     $shareImagesDirectoryItems = scandir('../../'.$config['shareImagesDirectory']);
-
     $disallowedDirectories = ['.', '..', '.DS_Store'];
     $shareImageFileNames = array_diff($shareImagesDirectoryItems, $disallowedDirectories);
 
@@ -37,13 +42,17 @@
 		<title>What the Hood?</title>
         <meta name='viewport' content='width=device-width, initial-scale=1'>
         <?php if ($shareImageMatched) { ?>
-            <meta property="og:title" content="What the Hood?" />
-            <meta property="og:url" content="http://whatthehood.city" />
+            <meta property='og:title' content='What the Hood?' />
+            <meta property='og:url' content='<?php echo $rootURL; ?>' />
             <meta property='og:image:url' content='../../<?php echo $config['shareImagesDirectory'].$shareImage['shareImageData']['fileNameMatched']; ?>' />
             <meta property='og:image' content='../../<?php echo $config['shareImagesDirectory'].$shareImage['shareImageData']['fileNameMatched']; ?>' />
         <?php } ?>
 	</head>
 	<body>
+
+    <script type='text/javascript'>
+        window.location.href = '<?php echo $rootURL; ?>';
+    </script>
 
 	</body>
 </html>
