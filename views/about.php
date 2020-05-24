41099a2729551005e28d2519a1e38ca20bed0a32
Leo Mancini <mancini@leo.gd>
2020-05-24 12:00:20 -0700

    Updating credits

--- views/about.php
+++ views/about.php
@@ -36,15 +36,23 @@
             <h3>Credits</h3>
             <br>
             <h6>New York City Neighborhoods Polygon Data</h6>
-            <span class='link'><a href='https://www.google.com/maps/d/u/1/viewer?hl=en&ll=40.70476551690573%2C-73.97829884999999&z=10&mid=1_gsxJNfmcGZI4ZL_7LnEHj72YpvgNq-w' title='NYC Neighborhoods Map' target='_blank'>NYC Neighborhoods Map</a></span>
+            <span class='link'><a href='https://www.google.com/maps/d/u/1/viewer?hl=en&ll=40.70476551690573%2C-73.97829884999999&z=10&mid=1_gsxJNfmcGZI4ZL_7LnEHj72YpvgNq-w' target='_blank' rel='noopener'>NYC Neighborhoods Map</a></span>
             <br>
             <br>
             <h6>New York City Thumbnail SVG</h6>
-            <span class='link'><a href='https://commons.wikimedia.org/wiki/File:5_Boroughs_Labels_New_York_City_Map.svg' title='via Wikimedia Commons' target='_blank'>Nafsadh</a> / Public domain</span>
+            <span class='link'><a href='https://commons.wikimedia.org/wiki/File:5_Boroughs_Labels_New_York_City_Map.svg' target='_blank' rel='noopener'>Nafsadh via Wikimedia Commons</a> / Public domain</span>
             <br>
             <br>
             <h6>San Francisco Thumbnail SVG</h6>
-            <span class='link'><a href='https://commons.wikimedia.org/wiki/File:San_Francisco_districts_map.svg' title='via Wikimedia Commons' target='_blank'>Peter Fitzgerald, OpenStreetMap [2]</a> / <a href='https://creativecommons.org/licenses/by-sa/2.0'>CC BY-SA</a></a>
+            <span class='link'><a href='https://commons.wikimedia.org/wiki/File:San_Francisco_districts_map.svg' target='_blank' rel='noopener'>Peter Fitzgerald, OpenStreetMap via Wikimedia Commons</a> / <a href='https://creativecommons.org/licenses/by-sa/2.0' target='_blank' rel='noopener'>CC BY-SA</a></span>
+            <br>
+            <br>
+            <h6>Background Maps</h6>
+            <span class='link'><a href='https://www.mapbox.com/' target='_blank'  rel='noopener'>Mapbox</a></span>
+            <br>
+            <br>
+            <h6>Icons</h6>
+            <span class='link'><a href='https://material.io/resources/icons/' target='_blank' rel='noopener'>Google Material Icons</a> / <a href='https://www.apache.org/licenses/LICENSE-2.0.html' target='_blank' rel='noopener'>Apache license version 2.0</a></span>
             <br>
             <br>
             <br>
