a945487d76b1e083320b75222161f233fd9978b8
Leo Mancini <mancini@leo.gd>
2020-06-07 13:50:14 -0700

    Fixing about page

--- resources/js/about.js
+++ resources/js/about.js
@@ -1,8 +1,6 @@
-let aboutScreenContentsPaddingTopAndBottom = 0;
-
 if (window.deviceType === 'desktop') {
     aboutScreenContentsPaddingTopAndBottom = 120;
-}
 
-document.getElementById('about').style.paddingTop = `${aboutScreenContentsPaddingTopAndBottom}px`;
-document.getElementById('about').style.paddingBottom = `${aboutScreenContentsPaddingTopAndBottom}px`;
+    document.getElementById('about').style.paddingTop = `${aboutScreenContentsPaddingTopAndBottom}px`;
+    document.getElementById('about').style.paddingBottom = `${aboutScreenContentsPaddingTopAndBottom}px`;
+}
